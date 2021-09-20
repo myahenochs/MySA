ec253d6debde21a1cc155408c9577607df2cf0ab
Myah <myahenochs@gmail.com>
2021-09-20 19:30:40 -0400

    Added files

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "cpu.hpp"
+#include "ram.hpp"
+#include "bus.hpp"
+
+int main(){
+
+    return 0;
+}
---
+++ register.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+//Still debating on whether I want this to be abstracted into a class or not.
+
+class Register{
+
+
+};
